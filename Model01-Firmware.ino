f6a26ca7b2388081456f0d3c938d05d42bb22a98
iliana weller <iweller@amazon.com>
2018-07-11 18:04:33 -0700

    Revert "Slow down DigitalRain"
    This reverts commit 01d7b737b83ae563c7895401c6e89e85ed5a0884.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -247,8 +247,6 @@ void setup() {
    */
   KeyboardHardware.maskKey(0, 0);
 
-  LEDDigitalRainEffect.DROP_TICKS = 36;
-
   HostOS.os(kaleidoscope::hostos::LINUX);
 
   Focus.addHook(FOCUS_HOOK_HOSTOS);
