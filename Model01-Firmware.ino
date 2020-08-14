7b10bf9ebcc30fa913a726ab50c58c7bf700c194
iliana etaoin <iliana@buttslol.net>
2020-08-13 23:50:27 -0700

    Any key is now Shift+Alt+Down

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -18,8 +18,7 @@
 #include "Kaleidoscope-PrefixLayer.h"
 
 
-enum { MACRO_ANY,
-       MACRO_WOBIPV4,
+enum { MACRO_WOBIPV4,
        MACRO_WOBIPV6,
        MACRO_HAMMER_AND_SICKLE,
        MACRO_TRANS,
@@ -45,11 +44,11 @@ KEYMAPS(
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),       Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___,
-   Key_Enter,          Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
-                       Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
-   ShiftToLayer(TMUX), Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
-   Key_RightShift,     Key_LeftAlt, Key_Spacebar, Key_RightControl,
+   LSHIFT(LALT(Key_DownArrow)), Key_6, Key_7, Key_8,     Key_9,         Key_0,         ___,
+   Key_Enter,                   Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                                Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   ShiftToLayer(TMUX),          Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   Key_RightShift,              Key_LeftAlt, Key_Spacebar, Key_RightControl,
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] = KEYMAP_STACKED
@@ -86,23 +85,8 @@ KEYMAPS(
 
 // *INDENT-ON*
 
-static void anyKeyMacro(uint8_t keyState) {
-  static Key lastKey;
-  bool toggledOn = false;
-  if (keyToggledOn(keyState)) {
-    lastKey.keyCode = Key_A.keyCode + (uint8_t)(millis() % 36);
-    toggledOn = true;
-  }
-
-  if (keyIsPressed(keyState))
-    kaleidoscope::hid::pressKey(lastKey, toggledOn);
-}
-
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   switch (macroIndex) {
-  case MACRO_ANY:
-    anyKeyMacro(keyState);
-    break;
   case MACRO_WOBIPV4:
     if (keyToggledOn(keyState))
       Macros.type(PSTR("209.251.245."));
