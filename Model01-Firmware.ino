f8d3884baf3814f8b3285b496cb35c85735334fd
iliana etaoin <iliana@buttslol.net>
2020-08-21 00:50:25 -0700

    Clean up a bunch of stuff I don't use

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -9,22 +9,15 @@
 #include "Kaleidoscope-LEDControl.h"
 #include "Kaleidoscope-LEDEffect-SolidColor.h"
 #include "Kaleidoscope-LEDEffect-DigitalRain.h"
-#include "Kaleidoscope-FocusSerial.h"
 #include "Kaleidoscope-Unicode.h"
 #include "Kaleidoscope-Syster.h"
 #include "Kaleidoscope-HostPowerManagement.h"
-#include "Kaleidoscope-MagicCombo.h"
 #include "Kaleidoscope-USB-Quirks.h"
 #include "Kaleidoscope-PrefixLayer.h"
 
 
-enum { MACRO_ANY,
-       MACRO_WOBIPV4,
+enum { MACRO_WOBIPV4,
        MACRO_WOBIPV6,
-       MACRO_HAMMER_AND_SICKLE,
-       MACRO_TRANS,
-       MACRO_RUST_DEBUG,
-       MACRO_SHRUG,
        MACRO_ZWS,
        MACRO_IMDS,
      }; // macros
@@ -38,14 +31,14 @@ static const kaleidoscope::plugin::PrefixLayer::dict_t prefixlayerdict[] PROGMEM
 KEYMAPS(
 
   [QWERTY] = KEYMAP_STACKED
-  (SYSTER,       Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+  (___,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
    Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
    ShiftToLayer(FUNCTION),
 
-   M(MACRO_ANY),       Key_6, Key_7, Key_8,     Key_9,         Key_0,         LSHIFT(LALT(Key_DownArrow)),
+   SYSTER,             Key_6, Key_7, Key_8,     Key_9,         Key_0,         LSHIFT(LALT(Key_DownArrow)),
    Key_Enter,          Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                        Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    ShiftToLayer(TMUX), Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
@@ -53,10 +46,10 @@ KEYMAPS(
    ShiftToLayer(FUNCTION)),
 
   [FUNCTION] = KEYMAP_STACKED
-  (___,      Key_F1,          Key_F2,         Key_F3,                     Key_F4,           Key_F5,           Key_LEDEffectPrevious,
-   Key_Tab,  ___,             ___,            ___,                        ___,              M(MACRO_TRANS),   ___,
-   Key_Home, ___,             M(MACRO_SHRUG), M(MACRO_RUST_DEBUG),        ___,              ___,
-   Key_End,  M(MACRO_ZWS),    ___,            M(MACRO_HAMMER_AND_SICKLE), M(MACRO_WOBIPV6), M(MACRO_WOBIPV4), ___,
+  (___,      Key_F1,       Key_F2, Key_F3, Key_F4,           Key_F5,           Key_LEDEffectPrevious,
+   Key_Tab,  ___,          ___,    ___,    ___,              ___,              ___,
+   Key_Home, ___,          ___,    ___,    ___,              ___,
+   Key_End,  M(MACRO_ZWS), ___,    ___,    M(MACRO_WOBIPV6), M(MACRO_WOBIPV4), ___,
    ___, Key_Delete, ___, ___,
    ___,
 
@@ -86,23 +79,8 @@ KEYMAPS(
 
 // *INDENT-ON*
 
-static void anyKeyMacro(uint8_t keyState) {
-  static Key lastKey;
-  bool toggledOn = false;
-  if (keyToggledOn(keyState)) {
-    lastKey.keyCode = Key_A.keyCode + (uint8_t)(millis() % 36);
-    toggledOn = true;
-  }
-
-  if (keyIsPressed(keyState))
-    kaleidoscope::hid::pressKey(lastKey, toggledOn);
-}
-
 const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   switch (macroIndex) {
-  case MACRO_ANY:
-    anyKeyMacro(keyState);
-    break;
   case MACRO_WOBIPV4:
     if (keyToggledOn(keyState))
       Macros.type(PSTR("209.251.245."));
@@ -110,51 +88,6 @@ const macro_t *macroAction(uint8_t macroIndex, uint8_t keyState) {
   case MACRO_WOBIPV6:
     if (keyToggledOn(keyState))
       Macros.type(PSTR("2620:fc:c000::"));
-    break;
-  case MACRO_HAMMER_AND_SICKLE:
-    if (keyToggledOn(keyState))
-      Unicode.type(0x262d);
-    break;
-  case MACRO_TRANS:
-    if (keyToggledOn(keyState))
-      Unicode.type(0x26a7);
-    break;
-  case MACRO_RUST_DEBUG:
-    // println!("{:?}", ); + left arrow x 2
-    return MACRODOWN(T(P), T(R), T(I), T(N), T(T), T(L), T(N),
-                     D(LeftShift), T(1), T(9), T(Quote), U(LeftShift),
-                     T(LeftCurlyBracket),
-                     D(LeftShift), T(Semicolon), T(Slash), U(LeftShift),
-                     T(RightCurlyBracket),
-                     D(LeftShift), T(Quote), U(LeftShift),
-                     T(Comma), T(Spacebar),
-                     D(LeftShift), T(0), U(LeftShift),
-                     T(Semicolon), T(LeftArrow), T(LeftArrow));
-    break;
-  case MACRO_SHRUG:
-    // Originally from algernon: https://github.com/algernon/Model01-sketch/blob/721187bc8f2205a4b6bf798377f4cfb79e4deb36/src/Leader.cpp#L52-L66
-    if (keyToggledOn(keyState)) {
-      bool markdown = kaleidoscope::hid::wasModifierKeyActive(Key_LeftShift) || kaleidoscope::hid::wasModifierKeyActive(Key_RightShift);
-      Unicode.type(0xaf);
-      if (markdown)
-        Macros.play(MACRO(Tc(Backslash), Tc(Backslash)));
-      Macros.play(MACRO(Tc(Backslash),
-                        D(RightShift),
-                        Tc(Minus),
-                        Tc(9),
-                        U(RightShift)));
-      Unicode.type(0x30c4);
-      Macros.play(MACRO(D(RightShift),
-                        Tc(0)));
-      if (markdown)
-        Macros.play(MACRO(U(RightShift),
-                          Tc(Backslash),
-                          D(RightShift)));
-      Macros.play(MACRO(Tc(Minus),
-                        U(RightShift),
-                        Tc(Slash)));
-      Unicode.type(0xaf);
-    }
     break;
   case MACRO_ZWS:
     if (keyToggledOn(keyState))
@@ -221,39 +154,6 @@ class SysterLED_ : public kaleidoscope::Plugin {
 };
 SysterLED_ SysterLED;
 
-class FocusVersionCommand_ : public kaleidoscope::Plugin {
- public:
-  FocusVersionCommand_() {}
-
-  kaleidoscope::EventHandlerResult onFocusEvent(const char *command) {
-    const char *cmd = PSTR("version");
-
-    if (::Focus.handleHelp(command, cmd))
-      return kaleidoscope::EventHandlerResult::OK;
-
-    if (strcmp_P(command, cmd) == 0) {
-      ::Focus.send(F(VERSION));
-      return kaleidoscope::EventHandlerResult::EVENT_CONSUMED;
-    }
-
-    return kaleidoscope::EventHandlerResult::OK;
-  }
-};
-FocusVersionCommand_ FocusVersionCommand;
-
-enum {
-  COMBO_TOGGLE_NKRO_MODE
-};
-
-static void toggleKeyboardProtocol(uint8_t combo_index) {
-  USBQuirks.toggleKeyboardProtocol();
-}
-
-USE_MAGIC_COMBOS({.action = toggleKeyboardProtocol,
-                  // Left Fn + Esc + Shift
-                  .keys = { R3C6, R2C6, R3C7 }
-                 });
-
 KALEIDOSCOPE_INIT_PLUGINS(
   Syster,
   LEDControl,
@@ -262,13 +162,8 @@ KALEIDOSCOPE_INIT_PLUGINS(
   LEDOff,
   HostOS,
   Macros,
-  Focus,
-  FocusHostOSCommand,
-  FocusVersionCommand,
   Unicode,
   HostPowerManagement,
-  MagicCombo,
-  USBQuirks,
   PrefixLayer,
   SysterLED
 );
@@ -276,11 +171,6 @@ KALEIDOSCOPE_INIT_PLUGINS(
 void setup() {
   Serial.begin(9600);
   Kaleidoscope.setup();
-
-  /*
-   * Work around for https://github.com/keyboardio/Kaleidoscope-Syster/issues/3
-   */
-  KeyboardHardware.maskKey(0, 0);
 
   HostOS.os(kaleidoscope::hostos::LINUX);
 
